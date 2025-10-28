32a886c70d51e4aeda4692282d31b1d4cfa75f30
Erick Moura <erick.andrew.moura@gmail.com>
2025-10-28 10:52:22 -0300

    Finish exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string gradeLetter;
+        Console.Write("Please, insert your grade percentage (number): ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+        
+        if (gradeLetter != "F"){
+            if ((grade % 10) >= 7 && grade < 90)
+            {
+                gradeLetter += "+";
+            }
+            if ((grade % 10) < 3) {
+                gradeLetter += "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations, you passed!");
+        } else
+        {
+            Console.WriteLine($"You do not passed, but please, try again!");
+        }
     }
 }
