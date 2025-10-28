718513111c2e40e0d094211030d94551576c9c7e
Erick Moura <erick.andrew.moura@gmail.com>
2025-10-28 18:33:53 -0300

    Finisehd exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Magic Number Game");
+        string again = "yes";
+
+        while (again == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int attemps = 0;
+            int answer;
+
+            do
+            {
+                Console.Write("What is the magic number? ");
+                answer = int.Parse(Console.ReadLine());
+                if (answer < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (answer > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                attemps++;
+            } while (answer != magicNumber);
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"{attemps} attemps!");
+            Console.Write($"Do you want to try again? ");
+            again = Console.ReadLine();
+        }
     }
 }
