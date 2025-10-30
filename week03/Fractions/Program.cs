5bc5454cf9fb2623138e018e06132ed16612b9be
Erick Moura <erick.andrew.moura@gmail.com>
2025-10-30 11:15:43 -0300

    Fractions Activity Finished

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        List<Fractions> fractions = new List<Fractions>();
+
+        Fractions f1 = new Fractions();
+        Fractions f2 = new Fractions(5);
+        Fractions f3 = new Fractions(3, 4);
+        Fractions f4 = new Fractions(1, 3);
+        
+        foreach (Fractions fract in fractions)
+        {
+            Console.WriteLine(fract.GetFractionString());
+            Console.WriteLine(fract.GetDecimalValue());
+        }
     }
 }
